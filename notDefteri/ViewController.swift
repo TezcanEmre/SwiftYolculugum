e03eab2ea8a1821b74e960b6045da5a2b60f3e96
M.Emre Tezcan <Tezcan_emre@icloud.com>
2023-06-08 00:51:25 +0300

    keyboard closing test

--- notDefteri/ViewController.swift
+++ notDefteri/ViewController.swift
@@ -49,6 +49,7 @@ class ViewController: UIViewController {
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
+        
     }
     @IBAction func deleteButtonClick(_ sender: Any) {
         
