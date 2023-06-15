b2ebb0f35333315d71ff725e56a6cea475848a34
M.Emre Tezcan <Tezcan_emre@icloud.com>
2023-06-16 01:14:00 +0300

    tableView Created

--- Perseus's Flight Book/Perseus's Flight Book/ViewController.swift
+++ Perseus's Flight Book/Perseus's Flight Book/ViewController.swift
@@ -7,10 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    @IBOutlet weak var tableView: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { // kaç row olacak
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {// row içindeki veriler
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "deneme"
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.h
     }
 
