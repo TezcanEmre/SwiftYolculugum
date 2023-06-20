96f17ac03f21e23f6a4dc80d986035613b8c3038
M.Emre Tezcan <Tezcan_emre@icloud.com>
2023-06-21 01:28:12 +0300

    added data for TableView

--- Perseus's Flight Book/Perseus's Flight Book/ViewController.swift
+++ Perseus's Flight Book/Perseus's Flight Book/ViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     @IBOutlet weak var tableView: UITableView!
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { // kaç row olacak
-        return 5
+        return aircraftsModels.count
     }
+    var aircraftsModels = [String]()
+    var aircraftsImages = [String]()
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {// row içindeki veriler
         let cell = UITableViewCell()
-        cell.textLabel?.text = "deneme"
+        cell.textLabel?.text = aircraftsModels[indexPath.row]
         return cell
     }
     
@@ -23,6 +25,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        aircraftsModels.append("Boeing 737")
+        aircraftsModels.append("Boeing 737 MAX")
+        aircraftsModels.append("Boeing 747")
+        aircraftsModels.append("Boeing 777")
+        aircraftsModels.append("Boeing 787")
+        aircraftsModels.append("Airbus A340")
+        
+        aircraftsImages.append("korean737")
+        aircraftsImages.append("lot737MAX")
+        aircraftsImages.append("luft747")
+        aircraftsImages.append("THY777")
+        aircraftsImages.append("eth787")
+        aircraftsImages.append("thyA340")
+        
+        
         // Do any additional setup after loading the view.h
     }
 
