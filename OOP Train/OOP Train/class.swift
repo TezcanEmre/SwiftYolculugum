e3639b1ff952f1228d32836d333920940bc98d6e
M.Emre Tezcan <Tezcan_emre@icloud.com>
2023-06-27 19:27:03 +0300

    enum added

--- OOP Train/OOP Train/class.swift
+++ OOP Train/OOP Train/class.swift
@@ -4,21 +4,28 @@
 //
 //  Created by Tezcan on 27.06.2023.
 //
+//enum gruplandırma için kullanılıyor detaylı bilgi https://hasanalidev.medium.com/swift-enum-yapısı-c1d9b22b0d96
 
 import Foundation
-class ogrenciler {
-    var name = ""
-    var surname = ""
-    var vize = 0
-    var final = 0
+enum studentType {
+    case sayisal
+    case sozel
+    case esitAgirlik
+}
+class studens {
+    var name : String
+    var surname : String
+    var vize : Int
+    var final : Int
+    var type: studentType
     
-    init(name: String, surname: String, vize: Int, final: Int) { //initializer (baslatici) ogrenciler sinifindaki degiskenlere veri atıyor
+    init(name: String, surname: String, vize: Int, final: Int, type: studentType) { //initializer (baslatici) ogrenciler sinifindaki degiskenlere veri atıyor
         print("init called") // komut satirinda kac defa baslayabildigini gormek icin
         self.name = name // class daki name e init deki name verisini atıyor benzer sekilde digerleri de
         self.surname = surname
         self.vize = vize
         self.final = final
-        
+        self.type = type 
         
     }
 }
--- OOP Train/OOP Train/main.swift
+++ OOP Train/OOP Train/main.swift
@@ -7,23 +7,28 @@
 
 import Foundation
 
-let ahmet = ogrenciler(name: "Ahmet", surname: "Cakir", vize: 85, final: 78) //tek satirda hem init cagirildi hem veriler atandi 
+let ahmet = studens(name: "Ahmet", surname: "Cakir", vize: 85, final: 78, type: .sayisal) //tek satirda hem init cagirildi hem veriler atandi
 
 print(ahmet.name)
 print(ahmet.surname)
 print(ahmet.vize)
 print(ahmet.final)
+print(ahmet.type)
 
-let bora = ogrenciler(name: "Bora", surname: "Kupcuk", vize: 15, final: 25)
+let bora = studens(name: "Bora", surname: "Kupcuk", vize: 15, final: 25, type: .sozel)
 
 print(bora.name)
 print(bora.surname)
 print(bora.vize)
 print(bora.final)
+print(bora.type)
 
-let ceren = ogrenciler(name: "Ceren", surname: "Ozcan", vize: 95, final: 60)
+let ceren = studens(name: "Ceren", surname: "Ozcan", vize: 95, final: 60, type: .esitAgirlik)
 print(ceren.name)
 print(ceren.surname)
 print(ceren.vize)
 print(ceren.final)
+print(ceren.type)
+
+
 
