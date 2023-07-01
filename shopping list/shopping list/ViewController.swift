f74687f1af3a3716a0cf1dcef03b152ba73d5f49
M.Emre Tezcan <Tezcan_emre@icloud.com>
2023-07-02 00:22:07 +0300

    created new project

---
+++ shopping list/shopping list/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  shopping list
+//
+//  Created by Tezcan on 2.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITabBarDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
