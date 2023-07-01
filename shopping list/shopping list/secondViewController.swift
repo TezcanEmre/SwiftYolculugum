0624c9a0addb2a3221ea4891b1be87e7d1019eeb
M.Emre Tezcan <Tezcan_emre@icloud.com>
2023-07-02 02:24:02 +0300

    main design

---
+++ shopping list/shopping list/secondViewController.swift
@@ -0,0 +1,28 @@
+//
+//  secondViewController.swift
+//  shopping list
+//
+//  Created by Tezcan on 2.07.2023.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var brandTextField: UITextField!
+    @IBOutlet weak var productTextLine: UITextField!
+    @IBOutlet weak var priceTextLine: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
