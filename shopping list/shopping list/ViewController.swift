5c991bf8acf3d99e9ecadc97dd604945652e26d6
M.Emre Tezcan <Tezcan_emre@icloud.com>
2023-07-10 00:46:22 +0300

    Added CoreData database and added some test objects

--- shopping list/shopping list/ViewController.swift
+++ shopping list/shopping list/ViewController.swift
@@ -6,27 +6,51 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITabBarDelegate, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return brandArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "demo"
+        cell.textLabel?.text = brandArray[indexPath.row]
         return cell
     }
-    
+    func dataFetch() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Shopping")
+        fetchRequest.returnsObjectsAsFaults = false //daha büyük veritabanıyla çalışırken caching den faydalanmak için kullanılıyor detaylı öğrenirim
+        do {
+            let sum = try context.fetch(fetchRequest)
+            for summary in sum as! [NSManagedObject] {
+                if let brand = summary.value(forKey: "seller") as? String {
+                    brandArray.append(brand)
+                }
+                if let id = summary.value(forKey: "id") as? UUID {
+                    idArray.append(id)
+                }
+                
+            }
+            tableView.reloadData()
+        } catch {
+            print("error")
+        }
+        
+    }
     
     @IBOutlet weak var tableView: UITableView!
-    
+    var brandArray = [String]()
+    var idArray = [UUID]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         tableView.delegate = self
         tableView.dataSource = self
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        dataFetch()
     }
     @objc func addButtonClicked() {
         performSegue(withIdentifier: "toSecondVC", sender: nil)
--- shopping list/shopping list/secondViewController.swift
+++ shopping list/shopping list/secondViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import CoreData
 
-class secondViewController: UIViewController {
+class secondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var brandTextField: UITextField!
     @IBOutlet weak var productTextLine: UITextField!
@@ -22,12 +23,51 @@ class secondViewController: UIViewController {
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(keyboardDisapper))
         view.addGestureRecognizer(gestureRecognizer)
         
+        
+        imageView.isUserInteractionEnabled = true
+        let imageGesture = UITapGestureRecognizer(target: self, action: #selector(imageChoose))
+        imageView.addGestureRecognizer(imageGesture)
     }
-    
+    @objc func imageChoose(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
     @objc func keyboardDisapper() {
         view.endEditing(true)
         
     }
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let shopping = NSEntityDescription.insertNewObject(forEntityName: "Shopping", into: context)
+        shopping.setValue(brandTextField.text!, forKey: "seller")
+        shopping.setValue(productTextLine.text!, forKey: "product")
+        if let price = Int(priceTextLine.text!) {
+            shopping.setValue(price, forKey: "price")
+        }
+        shopping.setValue(UUID(), forKey: "id")
+        let imgData = imageView.image!.jpegData(compressionQuality: 0.7)
+        shopping.setValue(imgData, forKey: "image")
+        
+        do {
+            try context.save()
+        } catch {
+        print("hata")
+        
+        }
+            
+            
+            
+            }
+    
+    
     
 
 }
