11d2b1af9f46c5263e46a2c99f549f4192e09e06
M.Emre Tezcan <Tezcan_emre@icloud.com>
2023-11-01 23:26:05 +0300

    -

--- Dog Lister/SecondViewController.swift
+++ Dog Lister/SecondViewController.swift
@@ -6,24 +6,75 @@
 //
 
 import UIKit
+import CoreData
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var DogNameTextField: UITextField!
+    @IBOutlet weak var DogAgeTextField: UITextField!
+    @IBOutlet weak var DogBreedTextField: UITextField!
+    @IBOutlet weak var OwnerTextField: UITextField!
+    @IBOutlet weak var VetTextField: UITextField!
+    @IBOutlet weak var OwnerPhoneTextField: UITextField!
+    @IBOutlet weak var VetPhoneTextField: UITextField!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //ViewDidLoad a herhangi bir yere tıklanınca klavyeyi gizlemesi için gesture recognizer
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+        ImageView.isUserInteractionEnabled = true
+        let imageGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(takeImage))
+        ImageView.addGestureRecognizer(imageGestureRecognizer)
+    }
+    @objc func takeImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = false
+        present(picker, animated: true, completion: nil)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        ImageView?.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func hideKeyboard() { //Gesture recognizer ın çalıştıracağı klavyeyi gizleyen objc kodu
+        view.endEditing(true)
+        
     }
-    */
+    @IBAction func SaveButtonClicked(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let Dog_Lister = NSEntityDescription.insertNewObject(forEntityName: "DogLister", into: context)
+        Dog_Lister.setValue(DogNameTextField.text!, forKey: "dogname")
+        if let age = Int(DogAgeTextField.text!) {
+            Dog_Lister.setValue(age, forKey: "dogage")
+        }
+        Dog_Lister.setValue(DogBreedTextField.text!, forKey: "dogbreed")
+        Dog_Lister.setValue(OwnerTextField.text!, forKey: "ownername")
+        Dog_Lister.setValue(OwnerPhoneTextField.text!, forKey: "ownertel")
+        Dog_Lister.setValue(VetTextField.text!, forKey: "vetname")
+        Dog_Lister.setValue(VetPhoneTextField.text!, forKey: "vettel")
+        Dog_Lister.setValue(UUID(), forKey: "id")
+        let image = ImageView.image!.jpegData(compressionQuality: 0.6)
+        Dog_Lister.setValue(image, forKey: "dogimage")
+        
+        do {
+            try context.save()
+            print("kaydedildi")
+        }
+        catch { print("hata")}
+    }
+    
+    
 
 }
--- Dog Lister/ViewController.swift
+++ Dog Lister/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //kaç sıra oluşacağını giriyoruz
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //hücrelerin isimlendirilmesinde kullanılıyor
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
+    @IBOutlet weak var TableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        TableView.delegate = self
+        TableView.dataSource = self
         // Do any additional setup after loading the view.
+        // navigation Controller'a veri ekleme butonu ekledim.
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        
+        
     }
+    func getData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
 
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "DogLister")
+        fetchRequest.returnsObjectsAsFaults = false //Caching de fayda sağlar büyük verilerde
+        do {
+           let gelenVeriler = try context.fetch(fetchRequest)
+            for gelenVeri in gelenVeriler as! [NSManagedObject] {
+                let dogname = gelenVeri.value(forKey: "dogname") as? String
+                print(dogname)
+                // en son burada kaldım komut satırında print etmiyor bakacağım
+            }
+        }
+        catch {
+            print("hata var")
+        }
+        
+    }
+    @objc func addButtonClicked () { //veri ekleme butonuna tıklanınca diğer VC ye geçen objc kodu
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
+        
+        
+    }
 
 }
 
